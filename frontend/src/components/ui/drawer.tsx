f66814fd23e85f23ab383970474c37240ff88e63
Malleswar-257 <sivanagamalleswar257@gmail.com>
2025-12-18 17:32:45 +0530

    🚀 Initial commit - Generated with CODE AGENT

--- frontend/src/components/ui/drawer.tsx
+++ frontend/src/components/ui/drawer.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { apiService } from '../services/api';
-
 import { Drawer as DrawerPrimitive } from "vaul@1.1.2";
 
 import { cn } from "./utils";
