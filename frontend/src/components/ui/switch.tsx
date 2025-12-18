e9ad5696330ef830ef041a810ba19738a3a5e976
Malleswar-257 <sivanagamalleswar257@gmail.com>
2025-12-18 16:00:49 +0530

    🚀 Initial commit - Generated with CODE AGENT

--- frontend/src/components/ui/switch.tsx
+++ frontend/src/components/ui/switch.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import * as React from "react";
+import { apiService } from '../services/api';
+
 import * as SwitchPrimitive from "@radix-ui/react-switch@1.1.3";
 
 import { cn } from "./utils";
